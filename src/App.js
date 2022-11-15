6dfdf8a413776c4106e75d4a275ccfa591efb005
kami-note <104632447+kami-note@users.noreply.github.com>
2022-11-15 11:34:34 -0300

    Final

---
+++ src/App.js
@@ -0,0 +1,46 @@
+var imagem = document.createElement("img");
+imagem.src = "src/gostoso.jpg";
+imagem.width = 80;
+imagem.height = 80;
+
+var p = document.createElement("h1");
+p.innerHTML = "Nada por enquanto tbm...";
+
+document.getElementById("conteudo").appendChild(p);
+document.getElementById("conteudo").appendChild(imagem);
+
+document.getElementById("btn-red").addEventListener("click", function() {
+    document.getElementById("titulo").setAttribute("class", "red")
+    document.getElementById("class").innerHTML = "Vermelho"
+});
+document.getElementById("btn-blue").addEventListener("click", function() {
+    document.getElementById("titulo").setAttribute("class", "blue")
+    document.getElementById("class").innerHTML = "Azul"
+});
+document.getElementById("btn-green").addEventListener("click", function() {
+    document.getElementById("titulo").setAttribute("class", "green")
+    document.getElementById("class").innerHTML = "Verde"
+});
+document.getElementById("btn-reset").addEventListener("click", function(){
+    document.getElementById("titulo").removeAttribute("class")
+    document.getElementById("class").innerHTML = "Padrão"
+});
+
+
+let nome = "Stefane"
+let cidade = "1928"
+
+switch(cidade){
+    default:
+        console.log("Não foi encontrado a minha cidade!")
+        break
+    case 'Salvador':
+        console.log(nome+", Sou de Salvador!")
+        break
+    case 'Recife':
+        console.log("Sou de "+ cidade + "!")
+        break
+
+}
+
+
